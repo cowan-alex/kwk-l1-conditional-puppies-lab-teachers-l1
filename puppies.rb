371744ccd696110af6ae4eaeb4d7dde2921a2015
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-07-04 05:10:40 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -15,6 +15,8 @@ def less_puppies(num)
     return "Good job, Rachel!"
   else
     return "Get back to your animal goals."
+  end
+end
 # 3. We've decided to give Rachel a little more control over her puppy obsession. Write a method called some_puppies that takes in two arguments - the number of puppies she has found and a maximum puppy allowance. This method should tell Rachel good job if she gets less than half her maximum allowance and to get back on her puppy restriction when she exceeds her maximum.
 
 
